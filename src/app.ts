efc6eccf668cba8796745fc7ce0e9b612a85cf78
Calmie <safiullinrafa@yandex.ru>
2025-01-20 22:01:31 +0300

    fix eslint conf and lint in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import userRouter from '../routes/users';
 import cardRouter from '../routes/cards';
 
 const app = express();
-const {PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/MestoDb');
 
@@ -12,10 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  //@ts-expect-error 2339
-  req.user = {
-    _id: '678e8b86b6511ce57f64ceeb'
-  };
+  //  @ts-expect-error 2339
+  req.user = { _id: '678e8b86b6511ce57f64ceeb' };
 
   next();
 });
