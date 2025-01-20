3521f6c4da6d7f98c68786fc6643f6eac2ad5e9f
Calmie <safiullinrafa@yandex.ru>
2025-01-20 20:21:46 +0300

    init

---
+++ controllers/cards.ts
@@ -0,0 +1,23 @@
+import {Request, Response } from 'express';
+import user from '../models/user';
+
+
+export const getUsers = (req: Request, res: Response) => {
+  user.find({})
+    .then(user => res.send({data: user}))
+    .catch(err => res.status(500).send({message: 'Произошла ошибка'}));
+};
+
+export const  getUser = (req: Request, res: Response) => {
+  user.findById(req.params.id)
+    .then(user => res.send({data: user}))
+    .catch(err => res.status(500).send({message: 'Произошла ошибка'}));
+};
+
+export const createUser = (req: Request, res: Response) => {
+  const {name, about, avatar} = req.body;
+
+  user.create({name, about, avatar})
+    .then(user => res.send({data: user}))
+    .catch(err => res.status(500).send({message: 'Произошла ошибка'}));
+};
