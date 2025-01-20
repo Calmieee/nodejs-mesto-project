e5738f34673217957eb42433d4fd60e436bb53b7
Calmie <safiullinrafa@yandex.ru>
2025-01-20 20:02:13 +0300

    init

---
+++ models/card.ts
@@ -0,0 +1,41 @@
+import { ObjectId } from 'mongoose';
+import mongoose from 'mongoose';
+
+export interface ICard {
+  name: string;
+  link: string;
+  owner: ObjectId;
+  likes: ObjectId[];
+  createdAt: Date;
+}
+
+const CardSchema = new mongoose.Schema<ICard>(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    likes: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+      default: [],
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+);
+
+export default mongoose.model<ICard>('card', CardSchema);
