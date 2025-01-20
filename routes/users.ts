3b21506c3f7ef5d09a4fd62929375953f1930b8d
Calmie <safiullinrafa@yandex.ru>
2025-01-20 20:26:32 +0300

    fix

--- routes/users.ts
+++ routes/users.ts
@@ -4,7 +4,7 @@ import { getUsers, getUser, createUser } from "../controllers/users";
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:userId', getUser);
 userRouter.post('/', createUser);
 
 export default userRouter;
